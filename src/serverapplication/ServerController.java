e8c82790ff22a4f5e65c06b57b09e4ae904e4026
Asmaa <asmaatarek93@gmail.com>
2020-01-26 14:35:24 +0200

    Add server UI

---
+++ src/serverapplication/ServerController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverapp;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+
+/**
+ *
+ * @author esma
+ */
+public class ServerController implements Initializable {
+    
+ @FXML
+ private Button graphButton;
+ @FXML
+ private Label statusLabel;
+  @FXML
+ private Label numOfUsersLabel;
+   @FXML
+ private Label numOfOnlineLabel;
+    @FXML
+ private Label numOfOfflineLabel;
+    @FXML
+ private ToggleButton serverToggleButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
